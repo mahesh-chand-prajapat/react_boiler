70dc105f452516a16e134a14f9d5caf1326cf75c
unknown <maheshpk678@gmail.com>
2023-09-24 23:17:31 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./header";
+
+function App() {
+  return (
+    <>
+    <h1 className="text-5xl">
+      Hello Worlds
+    </h1>
+    <Header />
+    </>
+  );
+}
+
+export default App;
